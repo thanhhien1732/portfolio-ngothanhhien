b89c24bbdd7f3f36ecb24f19fd9b7885a8bfe513
thanhhien1732 <thanhhien1732@gmail.com>
2026-06-18 19:33:10 +0700

    loại bỏ navbar ở chế độ mobile

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -44,7 +44,7 @@ function AppHeader() {
                     </span>
                 </Link>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
 
                 {/* <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
