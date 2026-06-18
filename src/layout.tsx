78c7ec96a843252b4e4b51e64bea5d3d403a852d
thanhhien1732 <thanhhien1732@gmail.com>
2026-06-18 19:26:49 +0700

    Tự động cuộn lên đầu trang mỗi khi đường dẫn thay đổi

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
+import { useEffect } from "react";
 import AppHeader from "./components/layout/app.header"
 import Nav from "./components/layout/Nav"
 
 function Layout() {
+  const location = useLocation();
+
+  // Tự động cuộn lên đầu trang mỗi khi đường dẫn (pathname) thay đổi
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return (
     <div>
